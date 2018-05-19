506e85009072c00d91dbd1650d22968a17caeeb8
ShahezadPanjwani <skpanjwani786@gmail.com>
2018-05-19 09:32:35 -0400

    updates

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES (' Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES (' Lime Burger', true);
